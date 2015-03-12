c4ba59e16351de92602aa7e269cb4f8e56aa50bc
local01 <cefnoram@gmail.com>
2015-03-12 14:21:33 -0600

    new and ok 12 Mar.

--- conf.py
+++ conf.py
@@ -138,7 +138,7 @@ pygments_style = 'sphinx'
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = "Home"
+html_short_title = 'Home'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
